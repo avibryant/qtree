624dbf8453d277a7c57195933d503a7e9958bfe7
Avi Bryant <avi@avibryant.com>
2013-06-19 21:15:09 -0700

    decay seems to work

--- lib/qtree/decay.rb
+++ lib/qtree/decay.rb
@@ -15,23 +15,34 @@ class DecayedQTree
   required :scaledtime, :int64, 1
   required :tree, QTree, 2
 
-  def self.create(value, timestamp = nil, level = nil)
+  def self.create(value, halflife, timestamp = nil, level = nil)
     timestamp ||= Time.new.to_i
     inst = self.new
-    inst.scaledtime = timestamp
+    inst.scaledtime = timestamp * Math.log(2.0)/halflife
     inst.tree = QTree.create(value, level)
     inst
   end
 
-  def merge(other, halflife, k = 48)
-    #todo
+  def merge(older, k = 48)
+    if older.scaledtime > scaledtime
+      older.merge(self, k)
+    else
+      inst = self.class.new
+      inst.scaledtime = scaledtime
+      inst.tree = tree.merge(older.tree, k, Math.exp(older.scaledtime - scaledtime))
+    end
+    inst
   end
 
   def quantile(p)
     tree.quantile(p)
   end
 
-  def range(from, to)
-    #todo
+  def mean
+    tree.mean
+  end
+
+  def size
+    tree.size
   end
 end
